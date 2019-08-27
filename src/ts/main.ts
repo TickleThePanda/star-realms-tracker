a36bb835e0ecd448c733950895f7453de9982129
Panda (Thomas) Attwood | TickleThePanda <thomas.pg.attwood@gmail.com>
2019-08-27 21:33:09 +0100

    Improve menu styling

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -8,9 +8,14 @@ import { GameController } from './GameController';
 window.customElements.define('player-controller', PlayerControllerElement);
 window.customElements.define('player-history', PlayerHistoryElement);
 
-window.addEventListener('load', () => {
+window.addEventListener('load', async () => {
   const START_PAGE: HTMLElement = document.getElementById('page__start');
   const GAME_PAGE: HTMLElement = document.getElementById('page__game');
+
+  const IN_GAME_MENU: HTMLElement = document.getElementById('in-game-menu');
+
+  const IN_GAME_MENU_TOGGLE_BUTTON: HTMLElement 
+      = document.getElementById('menu-toggle--in-game-menu');
 
   let game: Game = null;
   let controller: GameController = null;
@@ -39,8 +44,12 @@ window.addEventListener('load', () => {
 
     START_PAGE.hidden = false;
     GAME_PAGE.hidden = true;
+    IN_GAME_MENU.hidden = true;
   })
 
+  IN_GAME_MENU_TOGGLE_BUTTON.addEventListener('click', event => {
+    IN_GAME_MENU.hidden = !IN_GAME_MENU.hidden;
+  });
 
 });
 
